3085ae632f8f7b579dc8db020caa99e7c73eeb8a
akinl <akinlabisamson15@gmail.com>
2023-07-05 09:36:13 +0100

    pen

---
+++ app/controllers/findpu.controller.js
@@ -0,0 +1,45 @@
+const db = require("../models");
+const request = require("request");
+const User = db.user;
+const bill= db.bill;
+
+
+exports.fundback =  async (req, res) => {
+
+
+    try {
+
+
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
+            // req.session = null;
+            return res.status(200).send({status: "0", message: "User not found",});
+        }
+        const depo=await bill.findOne({
+            where: {
+                refid: req.body.refid,
+            },
+
+        });
+
+        if (!depo){
+            return res.status(200).send({status: "0", message: "transaction not found",});
+
+        }
+
+
+        return res.status(200).send({
+            status:"1",
+            pure:depo,
+        });
+
+    } catch (error) {
+        return res.status(201).send({
+            message: error.message});
+    }
+};
---
+++ app/controllers/reverseid.controller.js
@@ -0,0 +1,54 @@
+const db = require("../models");
+const request = require("request");
+const User = db.user;
+const bill= db.bill;
+
+
+exports.fundback =  async (req, res) => {
+
+
+    try {
+
+
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
+            // req.session = null;
+            return res.status(200).send({status: "0", message: "User not found",});
+        }
+        const depo=await bill.findOne({
+            where: {
+                refid: req.body.refid,
+            },
+
+        });
+
+        if (!depo){
+            return res.status(200).send({status: "0", message: "transaction not found",});
+
+        }
+
+        const cr=parseInt(user.wallet)+parseInt(depo.amount);
+        const user1 = await User.update(
+            { wallet: cr },
+            {
+                where: {
+                    username:user.username,
+                },
+
+            });
+
+        return res.status(200).send({
+            status:"1",
+            message:"Reverser Successful",
+        });
+
+    } catch (error) {
+        return res.status(201).send({
+            message: error.message});
+    }
+};
