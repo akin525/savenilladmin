fb25e2b94c0dce1c606a2f998414ffdaa5fabd34
DELL <akinlabisamson15@gmail.com>
2024-05-07 19:50:23 +0100

    rev

--- app/models/reverse.model.js
+++ app/models/reverse.model.js
@@ -1,33 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
     return sequelize.define("reverses", {
 
-        username: {
-            type: Sequelize.STRING
-        },
-        plan: {
-            type: Sequelize.STRING
-        },
-        amount: {
-            type: Sequelize.STRING
-        },
-        result: {
-            type: Sequelize.STRING
-        },
-        phone: {
-            type: Sequelize.STRING
-        },
-        refid: {
-            type: Sequelize.STRING
-        },
-        date: {
-            type: Sequelize.STRING
-        },
-        server_res: {
-            type: Sequelize.STRING
-        },
-        token: {
-            type: Sequelize.STRING
-        },
-
     });
 };
